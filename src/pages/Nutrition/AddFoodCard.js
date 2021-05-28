7a5833200052dcedeb32f80adb71e594ef4a71ac
surya-config <a1b2c3d4config@gmail.com>
2021-05-29 00:19:22 +0530

    added athlete add meal screen

---
+++ src/pages/Nutrition/AddFoodCard.js
@@ -0,0 +1,343 @@
+import React, { useState, useEffect } from "react";
+import { selectUserData } from "../../features/userSlice";
+import { useSelector } from "react-redux";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import TextField from "@material-ui/core/TextField";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 150,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
+
+const AddFoodCard = (props) => {
+  const classes = useStyles();
+  const userData = useSelector(selectUserData);
+  const [open, setOpen] = useState(props.item.foodName ? false : true);
+  //const [entireFood, setEntireFood] = useState(props.entireFood);
+  const [index, setIndex] = useState(props.index);
+  const [item, setItem] = useState(props.item);
+  const [idx, setIdx] = useState(props.idx);
+
+  useEffect(() => {
+    setIndex(props.index);
+    setIdx(props.idx);
+    setItem(props.item);
+  }, [props.index, props.item, props.idx]);
+
+  return (
+    <div className="athleteFoodCard">
+      {open ? (
+        <div className="athleteFoodCard__open">
+          <div>
+            <div className="athleteFoodCard__openContainer">
+              <h4>
+                {props.type === "non-editable"
+                  ? "Food Name"
+                  : "Search Food Name"}
+              </h4>
+              {props.type !== "non-editable" && (
+                <div
+                  onClick={() => {
+                    let temp = [...props.entireFood[index].food];
+                    let temp1 = [...props.entireFood];
+                    temp.splice(idx, 1);
+                    temp1[index].food = temp;
+                    props.setEntireFood(temp1);
+                  }}
+                >
+                  {/* <Icon
+              name="times"
+              type="font-awesome-5"
+              size={15}
+            /> */}{" "}
+                  X
+                </div>
+              )}
+            </div>
+            {props.type === "non-editable" ? (
+              <h4>{props.item.foodName}</h4>
+            ) : (
+              <div className="athleteAddMealfood__searchableDropdown">
+                <Autocomplete
+                  id="combo-box-demo"
+                  options={props.serverData}
+                  getOptionLabel={(option) => option.name}
+                  style={{ width: 300 }}
+                  inputValue={props.item.meal}
+                  onChange={(e, item) => {
+                    let foodData = [...props.entireFood];
+                    let temp = [...props.ent.food];
+                    temp[idx].foodName = item.name;
+                    temp[idx].proteins = item.protein * temp[idx].quantity || 0;
+                    temp[idx].carbs = item.carbs * temp[idx].quantity || 0;
+                    temp[idx].fat = item.fats * temp[idx].quantity || 0;
+                    // temp[idx].fibre = item.fibres;
+                    temp[idx].calories =
+                      item.calories * temp[idx].quantity || 0;
+                    temp[idx].foodDetails = item;
+                    temp[idx].serving = "";
+                    temp[idx].units = "";
+                    temp[idx].quantity = 1;
+
+                    foodData[index].food = temp;
+                    props.setEntireFood(foodData);
+                  }}
+                  renderInput={(params) => (
+                    <TextField
+                      {...params}
+                      label="Combo box"
+                      onChange={({ target }) => {
+                        let foodData = [...props.entireFood];
+                        let temp = [...props.ent.food];
+                        temp[idx].foodName = target.value;
+                        foodData[index].food = temp;
+                        props.setEntireFood(foodData);
+                      }}
+                      variant="outlined"
+                    />
+                  )}
+                />
+              </div>
+            )}
+          </div>
+
+          <div className="quantity-servings__container">
+            <div
+              style={{
+                alignItems: "flex-start",
+              }}
+            >
+              <h4>
+                {props.type === "non-editable" ? "Quantity" : "Enter Quantity"}
+              </h4>
+              <input
+                className="foodCard__quantInput"
+                value={String(props.item.quantity)}
+                onChange={(e) => {
+                  let foodData = [...props.entireFood];
+                  let temp = [...props.ent.food];
+                  if (e.target.value) {
+                    temp[idx].quantity = parseFloat(e.target.value);
+                  } else {
+                    temp[idx].quantity = "";
+                  }
+
+                  if (temp[idx].units === props.item.foodDetails?.units) {
+                    temp[idx].calories =
+                      (temp[idx].quantity * temp[idx].foodDetails?.calories) /
+                      temp[idx].foodDetails?.servings;
+                    temp[idx].proteins =
+                      (temp[idx].quantity * temp[idx].foodDetails?.protein) /
+                      temp[idx].foodDetails?.servings;
+                    temp[idx].carbs =
+                      (temp[idx].quantity * temp[idx].foodDetails?.carbs) /
+                      temp[idx].foodDetails?.servings;
+                    temp[idx].fat =
+                      (temp[idx].quantity * temp[idx].foodDetails?.fats) /
+                      temp[idx].foodDetails?.servings;
+                  } else if (
+                    temp[idx].units === props.item.foodDetails?.units2
+                  ) {
+                    temp[idx].calories =
+                      (temp[idx].quantity * temp[idx].foodDetails?.calories) /
+                      temp[idx].foodDetails?.servings2;
+                    temp[idx].proteins =
+                      (temp[idx].quantity * temp[idx].foodDetails?.protein) /
+                      temp[idx].foodDetails?.servings2;
+                    temp[idx].carbs =
+                      (temp[idx].quantity * temp[idx].foodDetails?.carbs) /
+                      temp[idx].foodDetails?.servings2;
+                    temp[idx].fat =
+                      (temp[idx].quantity * temp[idx].foodDetails?.fats) /
+                      temp[idx].foodDetails?.servings2;
+                  } else {
+                    temp[idx].proteins =
+                      temp[idx].foodDetails?.protein *
+                      (temp[idx].quantity || 0);
+                    temp[idx].carbs =
+                      temp[idx].foodDetails?.carbs * (temp[idx].quantity || 0);
+                    temp[idx].fat =
+                      temp[idx].foodDetails?.fats * (temp[idx].quantity || 0);
+                    temp[idx].calories =
+                      temp[idx].foodDetails?.calories *
+                      (temp[idx].quantity || 0);
+                  }
+                  foodData[index].food = temp;
+                  props.setEntireFood(foodData);
+                }}
+                placeholder={"Enter Quantity"}
+                editable={props.type === "non-editable" ? false : true}
+              />
+            </div>
+            <div className="foodCard__servingsInputContainer">
+              <FormControl className={classes.formControl}>
+                <InputLabel id="servings-select-label">
+                  Select Servings
+                </InputLabel>
+                <Select
+                  labelId="servings-select-label"
+                  id="servings-select-label"
+                  value={String(props.item.units)}
+                  onChange={(e) => {
+                    let foodData = [...props.entireFood];
+                    let temp = [...props.ent.food];
+                    temp[idx].units = e.target.value;
+                    if (temp[idx].units === props.item.foodDetails?.units) {
+                      temp[idx].calories =
+                        (temp[idx].quantity * temp[idx].foodDetails?.calories) /
+                        temp[idx].foodDetails?.servings;
+                      temp[idx].proteins =
+                        (temp[idx].quantity * temp[idx].foodDetails?.protein) /
+                        temp[idx].foodDetails?.servings;
+                      temp[idx].carbs =
+                        (temp[idx].quantity * temp[idx].foodDetails?.carbs) /
+                        temp[idx].foodDetails?.servings;
+                      temp[idx].fat =
+                        (temp[idx].quantity * temp[idx].foodDetails?.fats) /
+                        temp[idx].foodDetails?.servings;
+                    } else if (
+                      temp[idx].units === props.item.foodDetails?.units2
+                    ) {
+                      temp[idx].calories =
+                        (temp[idx].quantity * temp[idx].foodDetails?.calories) /
+                        temp[idx].foodDetails?.servings2;
+                      temp[idx].proteins =
+                        (temp[idx].quantity * temp[idx].foodDetails?.protein) /
+                        temp[idx].foodDetails?.servings2;
+                      temp[idx].carbs =
+                        (temp[idx].quantity * temp[idx].foodDetails?.carbs) /
+                        temp[idx].foodDetails?.servings2;
+                      temp[idx].fat =
+                        (temp[idx].quantity * temp[idx].foodDetails?.fats) /
+                        temp[idx].foodDetails?.servings2;
+                    } else {
+                      console.log("Nothing is selected");
+                    }
+                    foodData[index].food = temp;
+                    props.setEntireFood(foodData);
+                  }}
+                >
+                  {props.item?.foodDetails && (
+                    <MenuItem value={props.item?.foodDetails?.units}>
+                      {props.item?.foodDetails?.units}
+                    </MenuItem>
+                  )}
+                  {props.item?.foodDetails && (
+                    <MenuItem value={props.item?.foodDetails?.units2}>
+                      {props.item?.foodDetails?.units2}
+                    </MenuItem>
+                  )}
+                </Select>
+              </FormControl>
+            </div>
+          </div>
+          <div className="foodCard__macroNutrients">
+            <h4>Macro Nutrients</h4>
+            <div className="foodCard__macroNutrientsContainer">
+              <div className="foodCard__macroNutrient">
+                <h4>Proteins</h4>
+                <input
+                  className="foodCard__macroNutrientsInput"
+                  value={
+                    item.proteins
+                      ? String(Math.round(item.proteins * 10) / 10)
+                      : String(0)
+                  }
+                  editable={false}
+                />
+                <h5 style={{ marginHorizontal: 5, color: "black" }}>g</h5>
+              </div>
+              <div className="foodCard__macroNutrient">
+                <h4>Carbs</h4>
+                <input
+                  className="foodCard__macroNutrientsInput"
+                  value={
+                    item.carbs
+                      ? String(Math.round(item.carbs * 10) / 10)
+                      : String(0)
+                  }
+                  editable={false}
+                />
+                <h5 style={{ marginHorizontal: 5, color: "black" }}>g</h5>
+              </div>
+            </div>
+
+            <div className="foodCard__macroNutrientsContainer">
+              <div className="foodCard__macroNutrient">
+                <h4>Fat</h4>
+                <input
+                  className="foodCard__macroNutrientsInput"
+                  value={
+                    item.fat
+                      ? String(Math.round(item.fat * 10) / 10)
+                      : String(0)
+                  }
+                  editable={false}
+                />
+                <h5 style={{ marginHorizontal: 5 }}>g</h5>
+              </div>
+            </div>
+            <div className="foodCard__caloriesContainer">
+              <h4>Total Calories</h4>
+              <input
+                className="foodCard__caloriesInput"
+                value={
+                  item.calories
+                    ? String(Math.round(item.calories * 10) / 10)
+                    : String(0)
+                }
+                editable={false}
+              />
+              <h5 style={{ marginLeft: "5px" }}>kcal</h5>
+            </div>
+          </div>
+          <div
+            className="foodCard__check"
+            onClick={() => {
+              if (item.foodName && item.foodName != "") {
+                setOpen(false);
+              } else {
+                let temp = [...props.entireFood[index].food];
+                let temp1 = [...props.entireFood];
+                temp.splice(idx, 1);
+                temp1[index].food = temp;
+                props.setEntireFood(temp1);
+              }
+            }}
+          >
+            {/* <Icon
+              name="check"
+              type="font-awesome-5"
+              size={20}
+            /> */}
+            tick
+          </div>
+        </div>
+      ) : (
+        <div className="foodCard__checkAlternative">
+          <div onClick={() => setOpen(true)}>
+            <h5>{item.foodName}</h5>
+            {/* <Icon
+              name="chevron-down"
+              type="font-awesome-5"
+              size={20}
+            /> */}
+            down
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default AddFoodCard;
--- src/pages/Nutrition/AthleteAddMeal.js
+++ src/pages/Nutrition/AthleteAddMeal.js
@@ -1,12 +1,153 @@
-import React from "react";
-import "./Nutrition.css";
+import React, { useState, useEffect } from "react";
+import "./AthleteNutrition.css";
 import NutritionScreenHeader from "./NutritionScreenHeader";
+import TextField from "@material-ui/core/TextField";
+import Select from "@material-ui/core/Select";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import { makeStyles } from "@material-ui/core/styles";
+import AddFoodCard from "./AddFoodCard";
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 300,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
 
 function AthleteAddMeal() {
+  const classes = useStyles();
+  const [serverData, setServerData] = useState([]);
+  const [entireFood, setEntireFood] = useState([
+    {
+      meal: "",
+      description: "",
+      food: [
+        {
+          foodName: "",
+          proteins: 0,
+          carbs: 0,
+          fat: 0,
+          calories: 0,
+          quantity: 1,
+        },
+      ],
+    },
+  ]);
+  const [foodId, setFoodId] = useState("");
+  const [type, setType] = useState("");
+
+  useEffect(() => {
+    fetch("https://rongoeirnet.herokuapp.com/getFood")
+      .then((response) => response.json())
+      .then((responseJson) => {
+        //Successful response from the API Call
+        setServerData(responseJson.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <div className="athleteAddMeal">
       <NutritionScreenHeader name="Add Meal" />
-      <div className="athleteAddMeal__typeOfMeal"></div>
+      <div className="athleteAddMeal__typeOfMeal">
+        {entireFood.map((ent, index) => (
+          <div className="athleteAddMealfood__container">
+            <FormControl className={classes.formControl}>
+              <InputLabel id="meal-select-label">
+                Select the type of meal
+              </InputLabel>
+              <Select
+                labelId="meal-select-label"
+                id="meal-select-label"
+                value={ent.meal}
+                onChange={(e) => {
+                  let temp = [...entireFood];
+                  temp[index].meal = e.target.value;
+                  setEntireFood(temp);
+                }}
+              >
+                <MenuItem value={"Breakfast"}>Breakfast</MenuItem>
+                <MenuItem value={"Lunch"}>Lunch</MenuItem>
+                <MenuItem value={"Snack"}>Snack</MenuItem>
+                <MenuItem value={"Pre Workout"}>Pre Workout</MenuItem>
+                <MenuItem value={"Post Workout"}>Post Workout</MenuItem>
+                <MenuItem value={"Dinner"}>Dinner</MenuItem>
+              </Select>
+            </FormControl>
+
+            {ent.food?.map((item, idx) => {
+              return (
+                <AddFoodCard
+                  type={type}
+                  item={item}
+                  idx={idx}
+                  key={idx}
+                  ent={ent}
+                  entireFood={entireFood}
+                  index={index}
+                  serverData={serverData}
+                  setEntireFood={setEntireFood}
+                />
+              );
+            })}
+
+            <div className="foodCard__addButtons">
+              <div
+                className="foodCard__addfoodButton"
+                onClick={() => {
+                  let foodData = [...entireFood];
+                  let temp = [...ent.food];
+                  temp.push({
+                    foodName: "",
+                    proteins: 0,
+                    carbs: 0,
+                    fat: 0,
+                    calories: 0,
+                    quantity: 1,
+                  });
+                  foodData[index].food = temp;
+                  setEntireFood(foodData);
+                }}
+              >
+                <h3>Add Food</h3>
+              </div>
+
+              <div
+                className="foodCard__addmealButton"
+                onClick={() => {
+                  setEntireFood([
+                    ...entireFood,
+                    {
+                      meal: "",
+                      description: "",
+                      food: [
+                        {
+                          foodName: "",
+                          proteins: 0,
+                          carbs: 0,
+                          fat: 0,
+                          calories: 0,
+                          quantity: 1,
+                        },
+                      ],
+                    },
+                  ]);
+                }}
+              >
+                <h3>Add Meal</h3>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
